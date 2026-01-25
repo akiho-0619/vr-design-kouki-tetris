88415c33a1759f385fe9cde50107ee34a575670b
akiho-yamada <aki19.akiho@gmail.com>
2026-01-26 04:49:31 +0900

    とりあえず

--- scripts.js
+++ scripts.js
@@ -9,7 +9,8 @@ const minos = ["O", "I", "L", "J", "S", "Z", "T"];
 let drops = shuffle(minos);
 let sumDelays = 0;
 
-let droping = null;//drops.shift();
+let dropingMino = null;//drops.shift();
+let droping = null;
 
 
 let playersBoard = Array(20).fill().map(() => Array(10).fill(0));
@@ -23,7 +24,16 @@ create.ambientLight(
 create.directionalLight()
 helper.axes();
 // OILJSZT
-const colors = ["#ffde0a", "#00ddfa", "#0020f0", "#f07400", "#93f500", "#f52500", "#c505ff"];
+const COLORS = ["#ffde0a", "#00ddfa", "#0020f0", "#f07400", "#93f500", "#f52500", "#c505ff"];
+const MINO_OFFSET = {
+    "O":[[0,0],[1,0],[0,1],[1,1]],
+    "I":[[0,0],[0,1],[0,2],[0,3]],
+    "L":[[0,0],[0,1],[0,2],[1,0]],
+    "J":[[0,0],[0,1],[0,2],[-1,0]],
+    "S":[[0,0],[1,1],[0,1],[-1,0]],
+    "Z":[[0,0],[-1,1],[0,1],[1,0]],
+    "T":[[0,0],[-1,0],[0,1],[1,0]],
+}
 
 // player's board  
 for (let j=0; j<20; j++){
@@ -31,7 +41,7 @@ for (let j=0; j<20; j++){
         create.cube({
             position:[-3 + i*CUBE_SIZE, -1.5 + j*CUBE_SIZE, 0],
             size:SHADOW_CUBE_SIZE,
-            option:{color: colors[(20*j + i) % colors.length]},
+            option:{color: COLORS[(20*j + i) % COLORS.length]},
         });
     }
 }
@@ -42,7 +52,8 @@ for (let j=0; j<20; j++){
         create.cube({
             position:[3 - i*CUBE_SIZE, -1.5 + j*0.2, 0],
             size:SHADOW_CUBE_SIZE,
-            option:{color: colors[(20*j + i) % colors.length]},
+            option:{color: COLORS[(20*j + i) % COLORS.length]},
+            // rotation
         });
     }
 }
@@ -53,10 +64,10 @@ function createDropMino(type){
         case "O":
             mino = create.group({position: [-2, 0, 1],
                 children:[
-                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[0]}}),
-                    create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[0]}}),
-                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[0]}}),
-                    create.cube({position:[CUBE_SIZE, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[0]}}),
+                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[0]}}),
+                    create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[0]}}),
+                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[0]}}),
+                    create.cube({position:[CUBE_SIZE, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[0]}}),
                 ],
             });
             break;
@@ -64,10 +75,10 @@ function createDropMino(type){
         case "I":
             mino = create.group({position: [-1.5, 0, 1],
                 children:[
-                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[1]}}),
-                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[1]}}),
-                    create.cube({position:[0, 2*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[1]}}),
-                    create.cube({position:[0, 3*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[1]}}),
+                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[1]}}),
+                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[1]}}),
+                    create.cube({position:[0, 2*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[1]}}),
+                    create.cube({position:[0, 3*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[1]}}),
                 ],
             });
             break;
@@ -75,10 +86,10 @@ function createDropMino(type){
         case "L":
             mino = create.group({position: [-1, 0, 1],
                 children:[
-                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[2]}}),
-                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[2]}}),
-                    create.cube({position:[0, 2*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[2]}}),
-                    create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[2]}}),
+                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[2]}}),
+                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[2]}}),
+                    create.cube({position:[0, 2*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[2]}}),
+                    create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[2]}}),
                 ],
             });
             break;
@@ -86,10 +97,10 @@ function createDropMino(type){
         case "J":
             mino = create.group({position: [0, 0, 1],
                 children:[
-                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[3]}}),
-                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[3]}}),
-                    create.cube({position:[0, 2*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[3]}}),
-                    create.cube({position:[-CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[3]}}),
+                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[3]}}),
+                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[3]}}),
+                    create.cube({position:[0, 2*CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[3]}}),
+                    create.cube({position:[-CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[3]}}),
                 ],
             });
             break;
@@ -97,10 +108,10 @@ function createDropMino(type){
         case "S":
             mino = create.group({position: [0.5, 0, 1],
                 children:[
-                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[4]}}),
-                    create.cube({position:[CUBE_SIZE,CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[4]}}),
-                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[4]}}),
-                    create.cube({position:[-CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[4]}}),
+                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[4]}}),
+                    create.cube({position:[CUBE_SIZE,CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[4]}}),
+                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[4]}}),
+                    create.cube({position:[-CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[4]}}),
                 ],
             });
             break;
@@ -108,10 +119,10 @@ function createDropMino(type){
         case "Z":
             mino = create.group({position: [1.5, 0, 1],
                 children:[
-                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[5]}}),
-                    create.cube({position:[-CUBE_SIZE,CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[5]}}),
-                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[5]}}),
-                    create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[5]}}),
+                    create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[5]}}),
+                    create.cube({position:[-CUBE_SIZE,CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[5]}}),
+                    create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[5]}}),
+                    create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[5]}}),
                 ],
             });
             break;
@@ -119,10 +130,10 @@ function createDropMino(type){
         case "T":
             mino = create.group({position: [2.5, 0, 1],
             children:[
-                create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: colors[6]}}),
-                create.cube({position:[-CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[6]}}),
-                create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: colors[6]}}),
-                create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: colors[6]}}),
+                create.cube({position:[0, 0, 0], size:CUBE_SIZE, option:{color: COLORS[6]}}),
+                create.cube({position:[-CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[6]}}),
+                create.cube({position:[0, CUBE_SIZE, 0], size:CUBE_SIZE, option:{color: COLORS[6]}}),
+                create.cube({position:[CUBE_SIZE, 0, 0], size:CUBE_SIZE, option:{color: COLORS[6]}}),
             ],
         });
         break;
@@ -131,11 +142,58 @@ function createDropMino(type){
     return mino;
 }
 
+function placeMino(mino, rotate, boardX, boardY, board){
+    const STANDARD_X = Math.round((3 + boardX) / CUBE_SIZE);
+    const STANDARD_Y = Math.round((1.5 + boardY) / CUBE_SIZE);
+    const ROTATE_NUM = Math.round(rotate / (Math.PI / 2))%4; 
+    board[STANDARD_Y][STANDARD_X] = 1;
+    console.log(`Placing mino ${mino} at (${STANDARD_X}, ${STANDARD_Y}) rotated ${ROTATE_NUM} times`);
+    console.log(STANDARD_Y + MINO_OFFSET[mino][1][1], STANDARD_X + MINO_OFFSET[mino][1][0])
+    switch (ROTATE_NUM){
+        case 0:
+            for (let i = 1; i < 4; i++){
+                board[STANDARD_Y + MINO_OFFSET[mino][i][1]][STANDARD_X + MINO_OFFSET[mino][i][0]] = 1;
+            }
+            break;
+        case 1:
+            for (let i = 1; i < 4; i++){
+                board[STANDARD_Y + MINO_OFFSET[mino][i][0]][STANDARD_X - MINO_OFFSET[mino][i][1]] = 1;
+            }
+            break;
+        case 2:
+            for (let i = 1; i < 4; i++){
+                board[STANDARD_Y - MINO_OFFSET[mino][i][1]][STANDARD_X - MINO_OFFSET[mino][i][0]] = 1;
+            }
+            break;
+        case 3:
+            for (let i = 1; i < 4; i++){
+                board[STANDARD_Y - MINO_OFFSET[mino][i][0]][STANDARD_X + MINO_OFFSET[mino][i][1]] = 1;
+            }
+            break;
+    }
+
+    console.log(board.reverse().map(row => row.join(" ")).join("\n"));
+    /*
+
+    ## # #   #  ## ##   #
+    O# # #   # #O   O# #O#
+       # O# #O
+       O
+
+    S
+    -1, 0  0, +1  +1, +1
+    +1, 0  0, +1  +1, -1
+    +1, 0  0, -1  -1, -1
+    -1, 0  0, -1  -1, +1
+    */
+    return board;
+}
+
 animate(({delta, time})=>{
-    // a.position.lerp(new THREE.Vector3(-2.2, 2.3, 0), 0.1);
-    if (droping == null){
-        droping = createDropMino(drops.shift());
-        droping.position.set(-2.2, 2.3, 0);
+    if (dropingMino == null){
+        droping = drops.shift();
+        dropingMino = createDropMino(droping);
+        dropingMino.position.set(-2.2, 2.3, 0);
 
         if (drops.length == 0){
             drops = shuffle(minos);
@@ -143,12 +201,12 @@ animate(({delta, time})=>{
     }
     sumDelays += delta;
     if (sumDelays > drop_delays[dropStage]) {
-        if (droping.position.y > -1.5) {
-           droping.position.y -= 0.2;
+        if (dropingMino.position.y > -1.5) {
+           dropingMino.position.y -= 0.2;
         } else {
             // place the mino on the board
-            playersBoard[Math.round((1.5 + droping.position.y) / CUBE_SIZE)][Math.round((3 + droping.position.x) / CUBE_SIZE)] = 1;
-            droping = null;
+            playersBoard = placeMino(droping, dropingMino.rotation.z, dropingMino.position.x, dropingMino.position.y, playersBoard);
+            dropingMino = null;
         }
         sumDelays %= drop_delays[dropStage];
     }
@@ -165,30 +223,33 @@ function shuffle(array) {
 
 event.key.add((key, e) => {
     switch(key){
-        case "a":
-            droping.position.x += -CUBE_SIZE;
-            break;
-        case "d":
-            droping.position.x += CUBE_SIZE;
+        case "a":   // to left
+            dropingMino.position.x += -CUBE_SIZE;
+            dropingMino.position.x = Math.max(dropingMino.position.x, -3);
             break;
 
-        case "s":
-            if (droping.position.y > -1.5) {
-                droping.position.y -= 0.2;
-            } else {
+        case "d":   // to right
+            dropingMino.position.x += CUBE_SIZE;
+            dropingMino.position.x = Math.min(dropingMino.position.x, -3 + 9*CUBE_SIZE);
+            break;
+
+        case "s":   // soft drop
+            if (dropingMino.position.y > -1.5) {
+                dropingMino.position.y -= 0.2;
+            } else if (dropingMino != null) {
                 // place the mino on the board
-                playersBoard[Math.round((1.5 + droping.position.y) / CUBE_SIZE)][Math.round((3 + droping.position.x) / CUBE_SIZE)] = 1;
-                droping = null;
+                playersBoard = placeMino(droping, dropingMino.rotation.z, dropingMino.position.x, dropingMino.position.y, playersBoard);
+                dropingMino = null;
             }
             sumDelays %= drop_delays[dropStage];
             break;
 
-        case "q":
-            droping.rotation.z += Math.PI / 2;
+        case "q":   // rotate left
+            dropingMino.rotation.z += Math.PI / 2;
             break;
 
-        case "e":
-            droping.rotation.z += -Math.PI / 2;
+        case "e":   // rotate right
+            dropingMino.rotation.z += -Math.PI / 2;
             break;
     }
 });
